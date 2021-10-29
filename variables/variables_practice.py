113dcf9630958d53928af3fe0358d180d2c1b025
AmirMunlyn <77122385+AmirMunlyn@users.noreply.github.com>
2021-10-29 14:44:34 -0400

    version 0.2
    printed lives_remaining, forty_time, death_cry.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Amir Munyln, 10/29/21 2:39pm, version 0.1
+# Variables Practice, Amir Munyln, 10/29/21 2:42pm, version 0.2
 
 # Three Commmon Types of Data
 
@@ -6,3 +6,7 @@ lives_remaining = 6 # INTGERS, any +/- WHOLE number and 0.
 forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal.
 death_cry = "I am immortal. Impossible!!!"
 # Line 7 is a STRING -- collection of letters/numbers/other chars.
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
