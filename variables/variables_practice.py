ca9a0d7af54e57ec9f08fd308dce555585b3a023
AmirMunlyn <77122385+AmirMunlyn@users.noreply.github.com>
2021-10-29 14:39:51 -0400

    version 0.1
    Added three different strings: integers, floating point, and string.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Amir Munyln, 10/29/21 2:22pm, version 0.0 
+# Variables Practice, Amir Munyln, 10/29/21 2:39pm, version 0.1
+
+# Three Commmon Types of Data
+
+lives_remaining = 6 # INTGERS, any +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal.
+death_cry = "I am immortal. Impossible!!!"
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
